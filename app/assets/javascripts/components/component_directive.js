1703d7fbd17a8e32b3b6bd77ed15c00cffe392d7
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-22 20:34:13 -0700

    got propagation to work

--- app/assets/javascripts/components/component_directive.js
+++ app/assets/javascripts/components/component_directive.js
@@ -1,16 +1,37 @@
 app.directive('component', ['$compile', function($compile) {
 
+    var onKeyPress = function(event){
+     if (window.event.keyCode == 37 ){
+       // left arrow
+       console.log("left arrow");
+     } else if (window.event.keyCode == 39 ){
+       // right arrow
+       console.log("right arrow");
+     } else if (window.event.keyCode == 40 ){
+       // down arrow
+       console.log("down arrow");
+     } else if (window.event.keyCode == 38 ){
+       // up arrow
+       console.log("up arrow");
+     }
+   };
+
   return {
     restrict: "E",
     scope: {
       component: "="
     },
     link: function(scope, element, attrs){
-      scope.content = angular.element(scope.component.content);
       var template = scope.component.content;
       var linkFn = $compile(template);
       var content = linkFn(scope);
       element.html(content);
+
+      scope.onClick = function($event){
+        $event.stopPropagation();
+        angular.element($event.target)
+          .addClass('hovered');
+      };
     }
   };
 }]);
